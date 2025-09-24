46166bd6004dbc6813dc77323dadebc8bb14dec2
Mr-Watch <ssarantis2@gmail.com>
2025-09-25 02:12:46 +0300

    modified:   Shortcut Keys.user.js

--- Shortcut Keys.user.js
+++ Shortcut Keys.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Shortcut Keys
-// @version      1
+// @version      1.2
 // @description  TEST
 // @author       Mr-Watch
 // @match        https://eshoparmy.gr/wp-admin/post-new.php?post_type=product*
